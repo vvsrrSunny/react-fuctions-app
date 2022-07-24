3df9f74e7b30acf2ee23111eb7b2b7e016132e1f
vvsrrSunny <bharadwaj.varanasi96@gmail.com>
2022-07-24 23:21:58 +0800

    named slots feature #1

--- src/components/TableLayout.jsx
+++ src/components/TableLayout.jsx
@@ -1,11 +1,15 @@
 /* This example requires Tailwind CSS v2.0+ */
 
-// const Header = () => null
+const Header = () => null;
 
-// const Body = () => null
+const Body = () => null;
 
-export default function TableLayout({children}) {
+const TableLayout = ({ children }) => {
+    const header = children.find(el => el.type === Header);
+    const body = children.find(el => el.type === Body);
+console.log(children);
     return (
+
         <div className="px-4 sm:px-6 lg:px-8 text-left">
             <div className="sm:flex sm:items-center">
                 <div className="sm:flex-auto">
@@ -18,7 +22,7 @@ export default function TableLayout({children}) {
                     <button
                         type="button"
                         className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                        // onClick={() => children.addUser()}
+                    // onClick={() => children.addUser()}
                     >
                         Add user
                     </button>
@@ -28,11 +32,26 @@ export default function TableLayout({children}) {
                 <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                         <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                            {children}
+                            <table className="min-w-full divide-y divide-gray-300">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        {header ? header.props.children : null}
+                                    </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                    {body ? body.props.children : null}
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     )
 }
+
+TableLayout.Header = Header
+TableLayout.Body = Body
+
+export default TableLayout
--- src/components/UsersTable.jsx
+++ src/components/UsersTable.jsx
@@ -9,27 +9,24 @@ const people = [
 export default function UsersTable(props) {
     return (
         <TableLayout>
-        <table className="min-w-full divide-y divide-gray-300">
-            <thead className="bg-gray-50">
-                <tr>
-                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
-                        Name
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                        Title
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                        Email
-                    </th>
-                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                        Role
-                    </th>
-                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                        <span className="sr-only">Edit</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200 bg-white">
+            <TableLayout.Header>
+                <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                    Name
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Title
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Email
+                </th>
+                <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Role
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span className="sr-only">Edit</span>
+                </th>
+            </TableLayout.Header>
+            <TableLayout.Body>
                 {props.people.map((person) => (
                     <tr key={person.email}>
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
@@ -45,8 +42,7 @@ export default function UsersTable(props) {
                         </td>
                     </tr>
                 ))}
-            </tbody>
-        </table>
-        </TableLayout>
+            </TableLayout.Body>
+        </TableLayout >
     )
 }
